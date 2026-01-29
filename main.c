fed143048faf7d94fad375e552718871ce9860ed
Martin <koudela.m@icloud.com>
2026-01-29 08:01:34 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+struct product {
+    char name[100];
+    float price;
+    int quantity;
+    int inStock;
+
+};
+
+int main(void) {
+    printf("Hello, World!\n");
+    return 0;
+}
